b56b85aabaa5d8131419caf26471408e53238cf6
Jordan Gunti <jgunti1@gmail.com>
2023-01-30 15:21:48 -0600

    DOM Manipulation

---
+++ DOMManipulationandEvents.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('#container');
+const content = document.createElement('p');
+content.classList.add('content');
+content.textContent = 'Hey I\'m red'
+content.style.color = 'red';
+const header = document.createElement('h3');
+header.classList.add('header');
+header.textContent = 'I\'m a blue h3';
+header.style.cssText = 'color:blue;';
+const div = document.createElement('div');
+const header2 = document.createElement('h1');
+header2.textContent = 'I\m in a div';
+const paragraph2 = document.createElement('p');
+paragraph2.textContent = 'ME TOO!';
+div.style.cssText = 'border-style:solid; background-color:pink;';
+div.appendChild(header2);
+div.appendChild(paragraph2);
+container.appendChild(content);
+container.appendChild(header);
+container.appendChild(div);
