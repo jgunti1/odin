239dccfe246de684c3484b57b714aa84b0084a97
Jordan Gunti <jgunti1@gmail.com>
2024-12-09 15:04:07 -0600

    Small bugs that will need to be fixed at a later date

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -10,16 +10,16 @@ var calcScreen = document.getElementById('calculator-screen').value;
 var start = true; 
 /* Math Functions*/
 function add(numOne,numTwo) {
-    return (numOne + numTwo).toPrecision(7);
+    return (numOne + numTwo);
 }
 function subtract(numOne,numTwo) {
-    return (numOne - numTwo).toPrecision(7);
+    return (numOne - numTwo);
 }
 function multiply(numOne,numTwo) {
-    return (numOne * numTwo).toPrecision(7);
+    return (numOne * numTwo);
 }
 function divide(numOne,numTwo) {
-    return (numOne / numTwo).toPrecision(7);
+    return (numOne / numTwo);
 }
 
 //Operator function
@@ -38,6 +38,9 @@ function operate(numOne,numTwo,operator) {
         return divide(numOne,numTwo);
     }
 }
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
+}
 
 function populateScreen(pop) {
     if (start && pop != 'all-clear' ){
@@ -47,75 +50,110 @@ function populateScreen(pop) {
     }
     else {
         if (pop == '+' || pop == '-' || pop == '/' || pop == '*' && operator == '') {
+            //console.log("6");
             document.getElementById('calculator-screen').value += pop;
             operator = pop;
             document.getElementById("equals").disabled = false;
 
         }
         else if (pop == '=') {
-            if (operator != '' && second == ''){
-                total = operate(parseFloat(first), parseFloat(first), operator);
-                document.getElementById('calculator-screen').value = total;
-                first = total; 
-                second = ''
-                operator = '';
-                document.getElementById("equals").disabled = true;
+            //console.log("5");
+            if ((first == '0' && second == '0') || second == '0') {
+                document.getElementById('calculator-screen').value = 'stop';
+                console.log(first);
+                console.log(second);
+                console.log(operator);
             }
-            else if (operator == '') {
-
-            }
-            else { 
-                total = operate(parseFloat(first), parseFloat(second), operator);
-                document.getElementById('calculator-screen').value = total;
-                first = total; 
-                second = ''
-                operator = '';
-                document.getElementById("equals").disabled = true;
+            else {
+                if (operator != '' && second == ''){
+                    //console.log('5.1');
+                    total = operate(parseFloat(first), parseFloat(first), operator);
+                    if (isFloat(total)) {
+                        //console.log(total);
+                        total = total.toPrecision(7);
+                        document.getElementById('calculator-screen').value = total;
+                    }
+                    else {
+                        document.getElementById('calculator-screen').value = total;
+                    }
+                    first = total; 
+                    second = ''
+                    operator = '';
+                    document.getElementById("equals").disabled = true;
+                }
+                else if (operator == '') {
+                    //console.log('5.2');
+                }
+                else { 
+                    //console.log('5.3');
+                    console.log(first, second);
+                    total = operate(parseFloat(first), parseFloat(second), operator);
+                    if (isFloat(total)) {
+                        console.log(total);
+                        total = total.toPrecision(7);
+                        document.getElementById('calculator-screen').value = total;
+                    }
+                    else {
+                        document.getElementById('calculator-screen').value = total;
+                    }
+                    first = total; 
+                    second = ''
+                    operator = '';
+                    document.getElementById("equals").disabled = true;
+                }
             }
 
         }
         else if (pop == 'all-clear'){
+            //console.log("4");
             document.getElementById('calculator-screen').value = "0";
-            first = "";
-            second = "";
+            first = '';
+            second = '';
             document.getElementsByClassName("decimal")[0].disabled = false;
             document.getElementById("equals").disabled = false;
             start = true;
         }
         else if (pop == '.') {
+            //console.log("3");
             if (second == '') {
-                document.getElementById('calculator-screen').value += pop;
+                document.getElementById('calculator-screen').value += parseInt(pop);
                 document.getElementsByClassName("decimal")[0].disabled = true;
                 first += pop;
             }
             else {
-                document.getElementById('calculator-screen').value += pop;
+                document.getElementById('calculator-screen').value += parseInt(pop);
                 document.getElementsByClassName("decimal")[0].disabled = true;
                 second +=  pop;
             }
             
         }
         else if (operator == '') {
-            if (second == ''){
-                document.getElementById('calculator-screen').value += pop;
+            //console.log("1");
+            if (second == '' && document.getElementById('equals').disabled == false){
+                document.getElementById('calculator-screen').value += parseInt(pop);
                 first += pop; 
             }
             else {
+                document.getElementById('calculator-screen').value = parseInt(pop);
+                first = '';
                 first += pop;
-                document.getElementById('calculator-screen').value = pop;
             }
             
             
         }
         else {
-            if (document.getElementById("equals").disabled == true) {
-                document.getElementById('calculator-screen').value = pop;
-                second += pop;
+            //console.log("2");
+            if (second == '' && operator == '') {
+                // After a user enters an equation, they click any number
+                // we need to change the screen to be that number
+                // Order of events: 7, +, 1, equals, 8, 9
+                // 9 should clear the screen
+                document.getElementById('calculator-screen').value = parseInt(pop);
                 document.getElementsByClassName("decimal")[0].disabled = false;
             }
             else {
                 second += pop;
-                document.getElementById('calculator-screen').value = second;
+                document.getElementById('calculator-screen').value = parseInt(second);
                 document.getElementsByClassName("decimal")[0].disabled = false;
             }
         }
@@ -129,5 +167,6 @@ function isNumber(value) {
 buttons.forEach(function (i){
     i.addEventListener('click',function(){
         populateScreen(i.value);
-    })
+    });
+    
 })
